a619df94ea8165d2f108fae4c26ac161752a750e
Manish Jha <manishjha962@gmail.com>
2025-01-22 18:54:03 +0530

    implement routing with RouterPractice component and add About, Home, User, and UserDets pages

--- src/components/NavRoute.jsx
+++ src/components/NavRoute.jsx
@@ -5,7 +5,7 @@ const NavRoute = () => {
     return (
         <>
             <div className="flex">
-                <NavLink to="/" className="p-4">Home</NavLink>
+                <NavLink  to="/" className="p-4">Home</NavLink>
                 <NavLink to="/user" className="p-4">User</NavLink>
                 <NavLink to="/about" className="p-4">About</NavLink>
             </div>
